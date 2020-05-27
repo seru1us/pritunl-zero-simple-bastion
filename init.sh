a17394b1277852e1b62bd3d68176f06a72d1923d
Sean McCully <seancmccully@gmail.com>
2020-05-27 14:54:48 -0400

    adding stuff so passwords won't prompt

--- init.sh
+++ init.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 useradd bastion
+/usr/bin/ssh-keygen -A
 
 TRUSTED_PUBKEY=$(curl $TP_URL)
 
 sed -i '/^TrustedUserCAKeys/d' /etc/ssh/sshd_config
 sed -i '/^AuthorizedPrincipalsFile/d' /etc/ssh/sshd_config
 tee -a /etc/ssh/sshd_config << EOF
-
+ChallengeResponseAuthentication no
+PasswordAuthentication no
 TrustedUserCAKeys /etc/ssh/trusted
 AuthorizedPrincipalsFile /etc/ssh/principals
 EOF
@@ -19,5 +21,4 @@ tee /etc/ssh/trusted << EOF
 ssh-rsa $TRUSTED_PUBKEY
 EOF
 
-/usr/bin/ssh-keygen -A
 /usr/sbin/sshd -D -f /etc/ssh/sshd_config
