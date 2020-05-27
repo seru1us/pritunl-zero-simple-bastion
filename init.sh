f25eea81255e302125e40f55df6ae26e4ed3826a
Sean McCully <seancmccully@gmail.com>
2020-05-27 11:04:52 -0400

    amazon-ing it more

--- init.sh
+++ init.sh
@@ -2,9 +2,6 @@
 
 TRUSTED_PUBKEY=$(curl $TP_URL)
 
-ls -l /etc/sshd
-ls -l /etc/ssh
-
 sed -i '/^TrustedUserCAKeys/d' /etc/ssh/sshd_config
 sed -i '/^AuthorizedPrincipalsFile/d' /etc/ssh/sshd_config
 tee -a /etc/ssh/sshd_config << EOF
@@ -20,5 +17,4 @@ tee /etc/ssh/trusted << EOF
 ssh-rsa $TRUSTED_PUBKEY
 EOF
 
-systemctl restart sshd || true
-service sshd restart || true
+/usr/sbin/sshd -D -f /ssh/sshd_config
