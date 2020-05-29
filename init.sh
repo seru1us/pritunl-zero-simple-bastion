458f4c6dd92b4111fbfd4b08d1b485574f86834b
Sean McCully <seancmccully@gmail.com>
2020-05-29 17:33:28 -0400

    slowly adding the things back

--- init.sh
+++ init.sh
@@ -16,10 +16,10 @@ PasswordAuthentication no
 TrustedUserCAKeys /etc/ssh/trusted
 AuthorizedPrincipalsFile /etc/ssh/principals
 UsePAM no
-#ClientAliveInterval 120
-#ClientAliveCountMax 240
-#X11Forwarding no
-#AllowAgentForwarding no
+ClientAliveInterval 120
+ClientAliveCountMax 240
+X11Forwarding no
+AllowAgentForwarding no
 
 EOF
 tee /etc/ssh/principals << EOF
